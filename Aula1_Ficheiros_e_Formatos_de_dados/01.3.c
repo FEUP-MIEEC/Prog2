d0237f79e318be9f7dc0b3942b532b140ee3d366
Gonçalo Pereira <goncalo_pereira@outlook.pt>
2016-02-22 15:56:02 +0000

    Code Correction

--- Aula1_Ficheiros_e_Formatos_de_dados/01.3.c
+++ Aula1_Ficheiros_e_Formatos_de_dados/01.3.c
@@ -1,36 +1,31 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 int in_array(int a, int *v, int len){
-	int i=0;
-	for(i=0; i<=len; i++){
-		if(v[i]==a){
+	int i = 0;
+	for(i = 0; i <= len; i++){
+		if(v[i] == a){
 			return 1;
 		}
 	}
 	return 0;
 }
 
-
 int main(){
 	FILE *f;
-	int v_a[2]={131, 163};
-	int v_o[2]={149, 181};
-	int v_c[2]={135, 167};
-	f=fopen("lusiadas-utf8.txt", "r");
-	int c;
-	int freq[3]={};
-	char letras[3]={'A', 'O', 'C'};
+	int v_a[2] = {131, 163};
+	int v_o[2] = {149, 181};
+	int v_c[2] = {135, 167};
+	f = fopen("lusiadas-utf8.txt", "r");
+	int c = 0;
+	int freq[3] = {};
+	char letras[3] = {'A', 'O', 'C'};
 	int i;
-	while (1){
-		c=getc(f);
-		if (c==EOF){
-			break;
-		}
-		if (c==195){
-			c=getc(f); // Ler carater seguinte
+	while (c!=EOF){
+		c = getc(f);
+		if (c == 195){
+			c = getc(f);
 			if (in_array(c, v_a, sizeof(v_a)/sizeof(int))){
 				freq[0]++;
 			} else if(in_array(c, v_o, sizeof(v_o)/sizeof(int))){
@@ -40,11 +35,10 @@ int main(){
 			}
 		}
 	};
-	int nl=i;
+	int nl = i;
 	fclose(f);
-	for(i=0; i<2; i++){
+	for(i=0; i<2; i++){ // Neste caso apenas são dois, mas é uma boa prática fazê-lo
 		printf("Letra %c com til = %d\n", letras[i], freq[i]);
 	}
 	printf("Letra C com cedilha = %d\n", freq[2]);
-	
 }
