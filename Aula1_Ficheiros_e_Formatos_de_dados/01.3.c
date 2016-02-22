d6d33065aa3e268a18d5b815258be1e80469c998
Daniel Silva <daniel.@outlook.pt>
2016-02-22 15:41:03 +0000

    Submissão da segunda parte do problema 1

---
+++ Aula1_Ficheiros_e_Formatos_de_dados/01.3.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int in_array(int a, int *v, int len){
+	int i=0;
+	for(i=0; i<=len; i++){
+		if(v[i]==a){
+			return 1;
+		}
+	}
+	return 0;
+}
+
+void imprime_vetor(int *v, int len){
+	int i;
+	for(i=0; i<=len; i++){
+		
+	}
+}
+
+
+int main(){
+	FILE *f;
+	int v_a[2]={131, 163};
+	int v_o[2]={149, 181};
+	int v_c[2]={135, 167};
+	f=fopen("lusiadas-utf8.txt", "r");
+	int c;
+	int freq[3]={};
+	char letras[3]={'A', 'O', 'C'};
+	int i;
+	while (1){
+		c=getc(f);
+		if (c==EOF){
+			break;
+		}
+		if (c==195){
+			c=getc(f); // Ler carater seguinte
+			if (in_array(c, v_a, sizeof(v_a)/sizeof(int))){
+				freq[0]++;
+			} else if(in_array(c, v_o, sizeof(v_o)/sizeof(int))){
+				freq[1]++;
+			} else if(in_array(c, v_c, sizeof(v_c)/sizeof(int))){
+				freq[2]++;
+			}
+		}
+	};
+	int nl=i;
+	fclose(f);
+	for(i=0; i<2; i++){
+		printf("Letra %c com til = %d\n", letras[i], freq[i]);
+	}
+	printf("Letra C com cedilha = %d\n", freq[2]);
+	
+}
