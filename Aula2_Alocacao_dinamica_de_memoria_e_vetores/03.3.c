f7c27216623f1853171e8dbb53515358b61f06ad
Gonçalo Pereira <goncalo_pereira@outlook.pt>
2016-03-04 21:05:05 +0000

    Memory free on Aula 2

--- Aula2_Alocacao_dinamica_de_memoria_e_vetores/03.3.c
+++ Aula2_Alocacao_dinamica_de_memoria_e_vetores/03.3.c
@@ -5,30 +5,27 @@
 
 #define NSTRINGS 3
 
-int main1() {
+int main() {
     vetor *v;
     char str[100];
     int i, pos;
-    v= vetor_novo();
+    v = vetor_novo();
 
-    while(1)
-    {
-        for(i = 0; i < NSTRINGS; i++)
-		{
-			printf("[%d] ", i+1);
-			if(vetor_elemento(v, i)== NULL)
-				printf("(vazio)\n");
-			else
-            	printf("%s\n", vetor_elemento(v, i));
-		}
+    while (1) {
+        for (i = 0; i < NSTRINGS; i++) {
+            printf("[%d] ", i + 1);
+            if (vetor_elemento(v, i) == NULL)
+                printf("(vazio)\n");
+            else
+                printf("%s\n", vetor_elemento(v, i));
+        }
 
-		do
-		{
+        do {
             printf("Posicao para nova string (1 a %d): ", NSTRINGS);
             scanf("%d", &pos);
             getchar(); /* elimina \n */
-		}
-		while(pos < 0 || pos > NSTRINGS);
+        }
+        while (pos < 0 || pos > NSTRINGS);
 
         if (pos == 0)
             break;
@@ -38,6 +35,7 @@ int main1() {
         str[strlen(str) - 1] = '\0';
 
         vetor_insere(v, str, i);
+        vetor_apaga(v);
     }
     return 0;
 }
