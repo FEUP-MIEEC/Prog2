a51b20bc3d59045bedb82391c44e6bf88ab8d539
Daniel Silva <danny@dannyps.net>
2016-03-08 14:48:32 +0000

    I'll resume this insanity later

--- git.sh
+++ git.sh
@@ -1,3 +1,10 @@
+ exit 0
+ 
+ # Prog2  [![Build Status](https://travis-ci.org/FEUP-MIEEC/Prog2.svg?branch=master)](https://travis-ci.org/FEUP-MIEEC/Prog2)
+#Exercícios propostos/projetos nas aulas de Programação 2. 
+
+#Há uma cópia do repositório *master* [aqui](https://github.com/FEUP-MIEEC/Prog2/tree/build) (*build*), com o resultado da compilação de todos os exercícios, bem com os seus executáveis. Alguns contêm, ainda, o output produzido.
+
 
 git config --global user.name "Dannyps"
 git config --global user.email "danny@dannyps.net"
