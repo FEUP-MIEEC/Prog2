d8bcb650534c475f0b8cc1bf7f88ec9912a9897a
Daniel Silva <danny@dannyps.net>
2016-03-08 13:00:44 +0000

    Testing push :D

--- build.sh
+++ build.sh
@@ -11,3 +11,11 @@ do
 done
 
 
+git config --global user.name "Daniel Silva"
+git config --global user.email "danny@dannyps.net"
+git init
+git remote add origin https://spongy:${GH_TOKEN}@github.com/FEUP-MIEEC/Prog2 #>/dev/null
+git checkout --orphan gh-pages
+git add .
+git commit -q -m "Deploy $(date)" #&> /dev/null
+git push -q -f origin gh-pages #&> /dev/null
