0d92801b5c6c52ad9d4b0ed0f92f81d2b8ca506e
Daniel Silva <danny@dannyps.net>
2016-03-31 11:44:28 +0100

    Fix typo in build file

--- Aula5_Filas_e_Pilhas/build.sh
+++ Aula5_Filas_e_Pilhas/build.sh
@@ -5,6 +5,6 @@ echo "Begin" > out/aula_compile_output
 
 for i in 1 2
 do
-   cd $i; make; ./run; echo "Executing the compiled program..." ./run | tee out/run.outputcd ..
+   cd $i; make; ./run; echo "Executing the compiled program..." ./run | tee out/run.output; cd ..
 done
 cd ..
