c6bb000ee56b31a3ff63be0b2b17cff305de1401
Fábio Gaspar <fabiodanielreis@hotmail.com>
2016-04-01 20:48:22 +0100

    Exercicio 4

---
+++ Aula3_vetores_e_algoritmos_de_pesquisa_e_ordenacao/Extras/04/04.c
@@ -0,0 +1,126 @@
+#include "vetor.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void skipC(FILE * f, char c)
+{
+	while(getc(f) == (int)c);
+	fseek(f, -1, SEEK_CUR);
+}
+
+void readFile(char * filename, vetor * vec)
+{
+	int votes, year;
+	float rank;
+	char aux[200], c;
+	FILE * file;
+
+	file = fopen(filename, "r");
+	while(fgetc(file) != EOF)
+	{
+		fseek(file, 16, SEEK_CUR); 
+		/*A coluna 'New distribution' tem 16 carateres
+			Opcionalemnte podiamos fazer fscanf de um inteiro e desprezar o valor obtido.*/
+
+		fscanf(file, "%d", &votes); /* Ler numero de votos */
+		fscanf(file, "%f", &rank); /*Obter rank*/
+		
+		fseek(file, 2, SEEK_CUR); /*Avançar dois carateres, corresponde aos dois espaços entre Rank e Title*/
+		
+		fgets(aux, 200, file); /* Ler o título inclusive o ano: (yyyy)*/
+
+		/* 
+			*Obtem um apontador para a última ocorrência de um espaço 
+				que corresponde ao espaço entre título e ano <title> <year>
+			*Substituimos o espaço pelo carater Nulo. É uma forma de remover
+				o ano da string, que queremos que seja apenas o título do filme
+		*/
+		char * ptr = rindex(aux, (int)' '); 
+		*ptr = '\0';
+		
+		/*
+			Deslocamos a posição atual 7 carateres para trás
+			Nota: Ao fazer fgets, a posição atual corresponde ao '\n'
+			Então, os 7 carateres equivalem a: (yyyy)\n
+			De seguida podemos ler o ano
+		*/		
+		fseek(file, -7, SEEK_CUR);
+		fscanf(file, "%d", &year);
+
+		/* 
+			A posição atual corresponde ao carater ), após efetuado o fscanf()
+			Em todas as linhas, exceto a última, verifica-se a seguinte sequência: )\r\n
+			Vamos deslocar dois carateres para a frente
+				->Se o próximo é \n, signnifica que teremos mais elementos (filmes)
+				->Se não, é o fim do ficheiro, podemos parar o ciclo
+
+			TODO: A condição do fgetc() no while não está a funcionar devidamente, a última linha é lida duas vezes sem
+				o If abaixo; why??	
+		*/
+		fseek(file, 2, SEEK_CUR); 
+		vetor_insere(vec, aux, votes, rank, year, vec->tamanho);	
+		if (fgetc(file) != (int)'\n')
+			break;
+	}
+	
+	fclose(file);
+}
+
+void printArray(vetor * vec)
+{
+	if (vec == NULL || vec->tamanho == 0)
+		return;
+
+	for (int i = 0; i < vec->tamanho; ++i)
+	{
+		elemento el = vec->elementos[i];
+		printf("Votes:%d\tRank:%.1f\tYear:%d\tTitle:", el.votes, el.rank, el.year);
+		puts(el.title);
+	}
+}
+
+int main()
+{
+	vetor * v = vetor_novo();
+	vetor * results;
+	readFile("IMDB.txt", v);
+	
+	/* Leitura e preenchimento do vetor de filmes */
+	printf("[INICIO] TESTAR LEITURA DO FICHEIRO\n");
+	printArray(v);
+	printf("[FIM] TESTAR LEITURA DO FICHEIRO\n");
+	
+	/* Ordenação */
+	printf("[INICIO] ORDENAR O VETOR ALFABETICAMENTE\n");
+	if(!vetor_ordena(v))
+		printArray(v);
+	else{
+		printf("Ocorreu um erro\n");
+		return -1;
+	}
+	printf("[FIM] ORDENAR O VETOR ALFABETICAMENTE\n");
+	//
+	//
+	printf("[INICIO] ORDENAR O VETOR TENDO EM CONTA O 'ANO' (ORDEM CRESCENTE)\n");
+	if(!vetor_ordena_ano(v))
+		printArray(v);
+	else{
+		printf("Ocorreu um erro\n");
+		return -1;
+	}
+	printf("[FIM] ORDENAR O VETOR TENDO EM CONTA O 'ANO' (ORDEM CRESCENTE)\n");
+
+	printf("[INICIO] PESQUISAR E CRIAR VETOR COM TODOS OS FILMES DO ANO YYYY\n");
+	results = vetor_pesquisa_ano(v, 100);
+	if (results->tamanho == 0)
+		printf("Não foram encontrandas correspondências\n");
+	else
+		printArray(results);
+	printf("[FIM] PESQUISAR E CRIAR VETOR COM TODOS OS FILMES DO ANO YYYY\n");
+
+	vetor_apaga(v);
+	vetor_apaga(results);
+}
+
+
---
+++ Aula3_vetores_e_algoritmos_de_pesquisa_e_ordenacao/Extras/04/vetor.h
@@ -0,0 +1,89 @@
+/*****************************************************************/
+/*          Biblioteca vetor | PROG2 | MIEEC | 2014/15           */      
+/*****************************************************************/
+
+#ifndef VETOR_H
+
+typedef struct
+{
+	//votos	(int),	a	classificação	(float),	o	título	(string)	e	o	ano	(int)
+	int votes;
+	float rank;
+	char * title;
+	int year;
+    
+} elemento;
+
+/**
+* este registo contem um vetor de elementos, um contador do tamanho e outro para a capacidade
+*/
+typedef struct
+{
+	/** numero de elementos do vetor */
+	int tamanho;
+    
+    /** capacidade do vetor */
+    int capacidade;
+	
+    /** array de elementos armazenados */
+	elemento* elementos;
+    
+} vetor;
+
+/** 
+ *  cria um novo vetor com tamanho 0 e capacidade 0
+ *  retorno: apontador para o vetor criado
+ *  nota: se vetor nao foi criado retorna NULL
+ */
+vetor* vetor_novo();
+
+/** 
+ *  elimina um vetor, libertando toda a memoria ocupada
+ *  parametro: vec apontador para vetor
+ *  nota: se vec = NULL retorna sem fazer nada
+ */
+void vetor_apaga(vetor *vec);
+
+/**
+ *  indica o o numero de elementos do vetor
+ *  parametro: vec apontador para vetor
+ *  retorno: -1 se ocorrer algum erro ou numero de elementos do vetor
+ */
+int vetor_tamanho(vetor *vec);
+
+/**
+ *  insere um elemento na posicao especificada
+ *  parametro: vec apontador para vetor
+ *  parametro: valor string a inserir
+ *  parametro: pos indice do elemento onde se pretende inserir a string, se pos=tamanho insere no fim do vetor
+ *  retorno: -1 se ocorrer algum erro (p.ex. se valor pos indicar uma posicao invalida) ou o valor de pos se bem sucedido
+ *  nota: se a capacidade nao for suficiente para guardar novo elemento, devera ser aumentada para 2x em relacao a capacidade anterior; inicialmente aumenta para capacidade 1
+ */
+int vetor_insere(vetor* vec, const char* valor, int votes, float rank, int year, int pos);
+
+/**
+ *  devolve a posicao do elemento especificado
+ *  parametro: vec apontador para vetor
+ *  parametro: str string pretendida
+ *  retorno: posicao do elemento ou -1 se ocorrer algum erro ou nao encontrar elemento
+ */
+int vetor_pesquisa(vetor *vec, const char* str);
+
+/**
+ *  ordena um vetor por ordem alfabetica
+ *  parametro: vec apontador para vetor
+ *  retorno: -1 se ocorrer um erro ou 0 se for bem sucedido  
+ */
+int vetor_ordena(vetor *vec);
+
+/**
+ *  ordena um vetor por ordem crescente do ano
+ *  parametro: vec apontador para vetor
+ *  retorno: -1 se ocorrer um erro ou 0 se for bem sucedido  
+ */
+int vetor_ordena_ano(vetor* vec);
+
+vetor * vetor_pesquisa_ano(vetor* vec, int year);
+
+#define VETOR_H
+#endif
