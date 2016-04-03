86f16d919f312e5b5f375923e099f409a5da335a
Fábio Gaspar <Hyrokumata@users.noreply.github.com>
2016-04-03 17:08:48 +0100

    Small fix
    Alguém tem uma resolução melhor? Não estou muito satisfeito com esta..

--- Extras/Miniteste2-Exemplo/2/prob2.c
+++ Extras/Miniteste2-Exemplo/2/prob2.c
@@ -12,37 +12,46 @@ typedef struct {
 jogador* conta_vitorias(FILE* f, int *njogadores)
 {
         jogador * jog = malloc(sizeof(jogador));
+        if(jog == NULL)
+            return NULL;
+        
         char aux[15];
         int i, j;
         jogador tmp;
-        
         *njogadores = 0;
+        
 	while(fscanf(f, "%s", aux) == 1)
         {
             aux[strlen(aux)] = '\0';
-            puts(aux);
-            int found = 0;
+            int found = 0; /* Variável auxiliar */
+            /* Verifica se já existe no vetor um jogador com nome 'aux' */
             for(i = 0; i < *njogadores; i++)
             {
-                if(strcmp(aux, jog[i].nome) == 0)
+                if(strcmp(aux, jog[i].nome) == 0) /* Se encontrar ..*/
                 {
-                    jog[i].vitorias++;
+                    jog[i].vitorias++; /* Atualiza o número de vitórias */
                     found = 1;
-                    break;
+                    break; /* Termina o ciclo For */
                 }
             }
             
+            /* Se ainda não existe um jogador com nome 'aux', found = 0 */
             if(!found)
             {
+                /* Adiciona novo jogador */
                 jog = realloc(jog, (*njogadores + 1)*sizeof(jogador));
+                
+                if(jog == NULL)
+                    return NULL;
+                
                 strcpy(jog[i].nome, aux);
                 jog[i].vitorias = 1;
-                *njogadores = *njogadores + 1;
+                (*njogadores)++;
             }
             
         }
         
-        /* Ordenação */
+        /* Ordenação por inserção e ordem decrescente */
         for (i = 1; i < *njogadores; i++)
         {
 		tmp = jog[i];
