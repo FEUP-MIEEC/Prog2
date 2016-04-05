a4ba5b4aafef930798da20c518fc600927854eea
Daniel Silva <danny@dannyps.net>
2016-04-05 09:04:00 +0100

    Chalenge da Aula Prática 6 - turma 1

---
+++ Extras/Chalenges/ap6_t1.c
@@ -0,0 +1,31 @@
+#include <malloc.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n, i;
+    int begin, end;
+    while (1) {
+        scanf("%d", &n);
+        if (!n) exit(0);
+        int *b = malloc(sizeof(int) * (2*n));
+        begin=0;
+        end=n-1;
+        for (i = 0; i < n; ++i)
+            b[i] = i+1;
+        int m=n;
+        printf("Discarded cards: ");
+        while(m>2){
+            printf("%d, ", b[begin]);
+            begin++;
+            end++;
+            b[end]=b[begin];
+			begin++;
+            m--;
+        }
+		printf("%d", b[begin++]);
+		printf("\nRemaining card: ");
+    printf("%d\n\n", b[begin]);
+	
+    }
+}
