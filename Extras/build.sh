5498a57407faca41f06eb23b7797ec821b8d9589
Daniel Silva <danny@dannyps.net>
2016-04-06 15:27:41 +0100

    Small fix on build files

--- Extras/build.sh
+++ Extras/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-../figlet-2.2.5/figlet -d ../figlet-2.2.5/fonts " ==> Chalenges! "; cd Chalenges; chmod +x build.sh; ./build.sh; cd ..
+../figlet-2.2.5/figlet -d ../figlet-2.2.5/fonts " ╘═══> Chalenges! "; cd Chalenges; chmod +x build.sh; ./build.sh; cd ..
