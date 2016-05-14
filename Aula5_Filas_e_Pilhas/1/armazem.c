b9e9a1df6f705fd34813a37e41b885d6be239fc8
Gonçalo Pereira <goncalo_pereira@outlook.pt>
2016-05-14 02:20:43 +0100

    Aula 5 - Problema 1

--- Aula5_Filas_e_Pilhas/1/armazem.c
+++ Aula5_Filas_e_Pilhas/1/armazem.c
@@ -19,26 +19,35 @@ armazem* armazem_novo(int comprimento, int altura)
 /* alinea c) */
 int armazem_vazio(armazem* armz)
 {
-	/* devolve vazio */
-	return 1;
+	if(armz== NULL || fila_tamanho(armz->contentores)==0) return 1;
+	return 0;
 }
 
 /* alinea d) */
 int armazem_cheio(armazem* armz)
 {
-	/* devolve nao cheio */
+	if(armazem_vazio(armz)) return 0;
+	if(fila_tamanho(armz->contentores) == armz->comprimento && fila_back(armz->contentores)->tamanho == armz->altura) return 1;
 	return 0;
 }
 
-/* alinea e) */
+/* alinea e) */	
 int armazenar_contentor(armazem* armz, contentor* contr)
 {
-	/* devolve nao ha' espaco */
-	return 0;
+	pilha *p= pilha_nova();
+	if (armz == NULL || contr == NULL || armazem_cheio(armz)) return 0;
+	if (armazem_vazio(armz)) fila_push(armz->contentores, pilha_nova());
+	if(fila_back(armz->contentores)->tamanho== armz->altura) fila_push(armz->contentores, p);
+	pilha_push(fila_back(armz->contentores), contr);
+	return 1;
 }
 
 /* alinea f) */
 contentor* expedir_contentor(armazem* armz)
 {
-	return NULL;
+	if (armazem_vazio(armz) || pilha_top(fila_front(armz->contentores)) ==NULL) return NULL;
+	contentor *c = contentor_novo(pilha_top(fila_front(armz->contentores))->destino, pilha_top(fila_front(armz->contentores))->valor);
+	if(fila_front(armz->contentores)->tamanho <=1) fila_pop(armz->contentores);
+	else pilha_pop(fila_front(armz->contentores));
+	return c;
 }
