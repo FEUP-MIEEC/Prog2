14f67a92540ef3b82afa1d9d1c939ed9e8e86ecd
Fábio Gaspar <Hyrokumata@users.noreply.github.com>
2016-06-06 17:34:15 +0100

    Fixed

--- Extras/Miniteste2-Exemplo1/prob1/prob1.c
+++ Extras/Miniteste2-Exemplo1/prob1/prob1.c
@@ -6,28 +6,10 @@
 
 void avl_max_subarvore(arvore_avl *arv, const char *inicio)
 {
-	/*arvore_avl *a = avl_nova();
+	arvore_avl *a = avl_nova();
 	a->raiz = avl_pesquisa(arv, inicio);
-	printf("%s", avl_max(a));*/
-
-	if(!arv || !inicio)
-		return;
-
-	no_avl* node = avl_pesquisa(arv, inicio), *tmp;
-	if(!node)
-		return;
-
-	char *max = node->str;
-	while(node != NULL)
-	{
-		if(strcmp(node->str, max) > 0)
-			max = node->str;
-
-		node = node->direita;
-	}
-
-	printf("%s", max);	
-
+	printf("%s", avl_max(a));
+	free(a);
 }
 
 
