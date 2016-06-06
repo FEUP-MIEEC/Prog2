a1a2d0aecc8258ff0a22cf1bf08596e4cd6245c9
Gonçalo Pereira <goncalo_pereira@outlook.pt>
2016-06-06 10:45:01 +0100

    Folder Renaming

--- Aula09_Filas_de_Prioridade_e_Heaps/priorityqueue.c
+++ Aula09_Filas_de_Prioridade_e_Heaps/priorityqueue.c
@@ -1,196 +1,202 @@
-#define RAIZ 			(1)
-#define PAI(x) 			(x/2)
-#define FILHO_ESQ(x) 	(x*2)
-#define FILHO_DIR(x) 	((x*2)+1)
+#define RAIZ            (1)
+#define PAI(x)            (x/2)
+#define FILHO_ESQ(x)    (x*2)
+#define FILHO_DIR(x)    ((x*2)+1)
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include "vetor.h"
 
-typedef int (* compara)(elemento *, elemento *);
+typedef int (*compara)(elemento *, elemento *);
 
-int fp_insere(vetor * fp, char * valor, int prioridade) {
-	if (fp == NULL) return -1;
+int fp_insere(vetor *fp, char *valor, int prioridade) {
+    if (fp == NULL) return -1;
 
-	if (fp->tamanho_actual == fp->tamanho_maximo) return 0;
+    if (fp->tamanho_actual == fp->tamanho_maximo) return 0;
 
-	elemento *e = elemento_cria(prioridade, valor);
-	int i, j;
-	for (i = 0; i < fp->tamanho_actual; i++)
-		if (maior_que(fp->elementos[i], e) == 0)
-			break;
-	for(j = fp->tamanho_actual; j >= i; j--)
-		fp->elementos[j+1] = fp->elementos[j];
+    elemento *e = elemento_cria(prioridade, valor);
+    int i, j;
+    for (i = 0; i < fp->tamanho_actual; i++)
+        if (maior_que(fp->elementos[i], e) == 0)
+            break;
+    for (j = fp->tamanho_actual; j >= i; j--)
+        fp->elementos[j + 1] = fp->elementos[j];
 
-	fp->elementos[i] = e;
-	fp->tamanho_actual++;
-	return 1;
+    fp->elementos[i] = e;
+    fp->tamanho_actual++;
+    return 1;
 }
 
-char * fp_remove(vetor * fp) {
-	if(fp == NULL || fp->tamanho_actual ==0) return NULL;
-	elemento *e = fp->elementos[0];
-	int i;
+char *fp_remove(vetor *fp) {
+    if (fp == NULL || fp->tamanho_actual == 0) return NULL;
+    elemento *e = fp->elementos[0];
+    int i;
 
-	for(i = 0; i<fp->tamanho_actual; i++)
-		fp->elementos[i] = fp->elementos[i+1];
-	fp->tamanho_actual--;
-	return e->valor;
-	return NULL;
+    for (i = 0; i < fp->tamanho_actual; i++)
+        fp->elementos[i] = fp->elementos[i + 1];
+    fp->tamanho_actual--;
+    return e->valor;
+    return NULL;
 }
 
-void fp_carrega(vetor * fp, const char * filename) {
+void fp_carrega(vetor *fp, const char *filename) {
 
-	char texto[256];
-	int prioridade;
+    char texto[256];
+    int prioridade;
 
-	FILE * file = fopen(filename, "r");
-	if (!file) {
-		printf("Nao foi possivel abrir ficheiro!\n");
-		return;
-	}
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        printf("Nao foi possivel abrir ficheiro!\n");
+        return;
+    }
 
-	while(!feof(file)) {
-		fscanf(file, "%d ", &prioridade);
-		fgets(texto, 256, file);
-		texto[strlen(texto)-1] = '\0';
-		fp_insere(fp, texto, prioridade);
-	}
+    while (!feof(file)) {
+        fscanf(file, "%d ", &prioridade);
+        fgets(texto, 256, file);
+        texto[strlen(texto) - 1] = '\0';
+        fp_insere(fp, texto, prioridade);
+    }
 
-	fclose(file);
+    fclose(file);
 }
 
 
-int heap_insere(vetor * h, compara cmp, char * texto, int prioridade) {
-
-	return 1;
+int heap_insere(vetor *h, compara cmp, char *texto, int prioridade) {
+    int i, j;
+    if (h->tamanho_actual == h->tamanho_maximo) return 0;
+    elemento *e = elemento_cria(prioridade, texto);
+    for (i = 0; i < h->tamanho_actual && cmp(h->elementos[i], e) != 1; i++);
+    for (j = i; j > 0; j--) h->elementos[j + 1] = h->elementos[j];
+    h->elementos[i] = e;
+    h->tamanho_actual++;
+    return i;
 }
 
-char * heap_remove(vetor * h, compara cmp) {
+char *heap_remove(vetor *h, compara cmp) {
 
-	return NULL;
+    return NULL;
 }
 
-void heap_carrega(vetor * v, compara cmp, const char * filename) {
+void heap_carrega(vetor *v, compara cmp, const char *filename) {
 
-	char texto[256];
-	int prioridade;
+    char texto[256];
+    int prioridade;
 
-	FILE * file = fopen(filename, "r");
-	if (!file) {
-		printf("Nao foi possivel abrir ficheiro!\n");
-		return;
-	}
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        printf("Nao foi possivel abrir ficheiro!\n");
+        return;
+    }
 
-	while(!feof(file)) {
-		fscanf(file, "%d ", &prioridade);
-		fgets(texto, 256, file);
-		texto[strlen(texto)-1] = '\0';
-		heap_insere(v, cmp, texto, prioridade);
-	}
+    while (!feof(file)) {
+        fscanf(file, "%d ", &prioridade);
+        fgets(texto, 256, file);
+        texto[strlen(texto) - 1] = '\0';
+        heap_insere(v, cmp, texto, prioridade);
+    }
 
-	fclose(file);
+    fclose(file);
 }
 
 
 int main(int argc, char **argv) {
-	vetor * v = vetor_cria(42);
-	int i;
+    vetor *v = vetor_cria(42);
+    int i;
 
-	printf("Teste da implementacao de fila de prioridade com vetor\n");
+    printf("Teste da implementacao de fila de prioridade com vetor\n");
 
-	fp_carrega(v, "dados.txt");
-	printf("Comparacoes necessarias para carregar o ficheiro: %d.\n", ncomparacoes);
+    fp_carrega(v, "dados.txt");
+    printf("Comparacoes necessarias para carregar o ficheiro: %d.\n", ncomparacoes);
 
-	vetor_imprime(v);
+    vetor_imprime(v);
 
-	if (v->tamanho_actual != 42) {
-		printf("Tamanho actual do vetor devia ser 42!\n");
-		return 1;
-	}
+    if (v->tamanho_actual != 42) {
+        printf("Tamanho actual do vetor devia ser 42!\n");
+        return 1;
+    }
 
-	if (strcmp("Maurice V. Wilkes", fp_remove(v))) {
-		printf("[ERRO] Elemento com maior prioridade devia ser \"Maurice V. Wilkes\".\n");
-		return 1;
-	}
+    if (strcmp("Maurice V. Wilkes", fp_remove(v))) {
+        printf("[ERRO] Elemento com maior prioridade devia ser \"Maurice V. Wilkes\".\n");
+        return 1;
+    }
 
-	ncomparacoes = 0;
-	fp_remove(v);
-	printf("Comparacoes necessarias para remover um valor: %d.\n", ncomparacoes);
+    ncomparacoes = 0;
+    fp_remove(v);
+    printf("Comparacoes necessarias para remover um valor: %d.\n", ncomparacoes);
 
-	ncomparacoes = 0;
-	fp_insere(v, "Novo elemento", 99);
-	printf("Comparacoes necessarias para inserir valor com maxima prioridade: %d.\n", ncomparacoes);
-	fp_remove(v);
+    ncomparacoes = 0;
+    fp_insere(v, "Novo elemento", 99);
+    printf("Comparacoes necessarias para inserir valor com maxima prioridade: %d.\n", ncomparacoes);
+    fp_remove(v);
 
-	ncomparacoes = 0;
-	fp_insere(v, "Novo elemento", 0);
-	printf("Comparacoes necessarias para inserir valor com minima prioridade: %d.\n", ncomparacoes);
-	fp_remove(v);
+    ncomparacoes = 0;
+    fp_insere(v, "Novo elemento", 0);
+    printf("Comparacoes necessarias para inserir valor com minima prioridade: %d.\n", ncomparacoes);
+    fp_remove(v);
 
-	ncomparacoes = 0;
-	fp_insere(v, "Novo elemento", 42);
-	printf("comparacoes necessarias para inserir valor com media prioridade: %d.\n", ncomparacoes);
+    ncomparacoes = 0;
+    fp_insere(v, "Novo elemento", 42);
+    printf("comparacoes necessarias para inserir valor com media prioridade: %d.\n", ncomparacoes);
 
-	/******* 2 *******/
+    /******* 2 *******/
 
-	printf("\n\nTeste da implementacao de fila de prioridade com heap\n");
+    printf("\n\nTeste da implementacao de fila de prioridade com heap\n");
 
-	vetor * v1 = vetor_criaHeap(42);
-	int (* compara1)(elemento *, elemento *);
-	compara1 = maior_que;
+    vetor *v1 = vetor_criaHeap(42);
+    int (*compara1)(elemento *, elemento *);
+    compara1 = maior_que;
 
-	heap_carrega(v1, compara1, "dados.txt");
-	printf("comparacoes necessarias para carregar o ficheiro: %d.\n", ncomparacoes);
+    heap_carrega(v1, compara1, "dados.txt");
+    printf("comparacoes necessarias para carregar o ficheiro: %d.\n", ncomparacoes);
 
-	vetor_imprimeHeap(v1);
+    vetor_imprimeHeap(v1);
 
-	if (v1->tamanho_actual != 42) {
-		printf("Tamanho actual do vetor deviar ser 42!\n");
-		return 1;
-	}
+    if (v1->tamanho_actual != 42) {
+        printf("Tamanho actual do vetor deviar ser 42!\n");
+        return 1;
+    }
 
-	if (strcmp("Maurice V. Wilkes", heap_remove(v1, compara1))) {
-		printf("[ERRO] Elemento com maior prioridade devia ser \"Maurice V. Wilkes\".\n");
-		return 1;
-	}
+    if (strcmp("Maurice V. Wilkes", heap_remove(v1, compara1))) {
+        printf("[ERRO] Elemento com maior prioridade devia ser \"Maurice V. Wilkes\".\n");
+        return 1;
+    }
 
-	ncomparacoes = 0;
-	heap_remove(v1, compara1);
-	printf("comparacoes necessarias para remover um valor: %d.\n", ncomparacoes);
+    ncomparacoes = 0;
+    heap_remove(v1, compara1);
+    printf("comparacoes necessarias para remover um valor: %d.\n", ncomparacoes);
 
-	ncomparacoes = 0;
-	heap_insere(v1, compara1, "Novo elemento", 99);
-	printf("comparacoes necessarias para inserir valor com maxima prioridade: %d.\n", ncomparacoes);
+    ncomparacoes = 0;
+    heap_insere(v1, compara1, "Novo elemento", 99);
+    printf("comparacoes necessarias para inserir valor com maxima prioridade: %d.\n", ncomparacoes);
 
-	heap_remove(v1, compara1);
+    heap_remove(v1, compara1);
 
-	ncomparacoes = 0;
-	heap_insere(v1, compara1, "Novo elemento", 0);
-	printf("comparacoes necessarias para inserir valor com minima prioridade: %d.\n", ncomparacoes);
-	heap_remove(v1, compara1);
+    ncomparacoes = 0;
+    heap_insere(v1, compara1, "Novo elemento", 0);
+    printf("comparacoes necessarias para inserir valor com minima prioridade: %d.\n", ncomparacoes);
+    heap_remove(v1, compara1);
 
-	ncomparacoes = 0;
-	heap_insere(v1, compara1, "Novo elemento", 42);
-	printf("comparacoes necessarias para inserir valor com media prioridade: %d.\n", ncomparacoes);
+    ncomparacoes = 0;
+    heap_insere(v1, compara1, "Novo elemento", 42);
+    printf("comparacoes necessarias para inserir valor com media prioridade: %d.\n", ncomparacoes);
 
 
-	/******* 3 *******/
+    /******* 3 *******/
 
-	v1 = vetor_criaHeap(42);
-	int (* compara)(elemento * e1, elemento * e2);
-	compara = maior_que_HeapSort;
+    v1 = vetor_criaHeap(42);
+    int (*compara)(elemento *e1, elemento *e2);
+    compara = maior_que_HeapSort;
 
-	heap_carrega(v1, compara, "dados.txt");
-	printf("comparacoes necessarias para carregar o ficheiro: %d.\n", ncomparacoes);
+    heap_carrega(v1, compara, "dados.txt");
+    printf("comparacoes necessarias para carregar o ficheiro: %d.\n", ncomparacoes);
 
-	char *ordenados[42];
+    char *ordenados[42];
 
-	for(i = 0; i < 42; i++)
-	  printf("%s\n", heap_remove(v1, compara));
+    for (i = 0; i < 42; i++)
+        printf("%s\n", heap_remove(v1, compara));
 
-	return 0;
+    return 0;
 }
 
 
--- Extras/Miniteste2-Exemplo2/prob2/prob2.c
+++ Extras/Miniteste2-Exemplo2/prob2/prob2.c
@@ -5,9 +5,8 @@
 #include "grafo.h"
 #include "lista.h"
 
-lista* descobre_caminho (grafo *g, int origem, int destino, int obrigatorio)
-{
-	/* prob 2.1 - a implementar */
+lista *descobre_caminho(grafo *g, int origem, int destino, int obrigatorio) {
+    /* prob 2.1 - a implementar */
     lista *lst = lista_nova();
     int size, *c1, *c2, i;
     char s[3];
@@ -24,87 +23,85 @@ lista* descobre_caminho (grafo *g, int origem, int destino, int obrigatorio)
     return lst;
 }
 
-int simula_acontecimentos(lista *acoes, lista *tempos, int n)
-{
+int simula_acontecimentos(lista *acoes, lista *tempos, int n) {
     /* prob 2.2 - a implementar */
+    if (n > 20) return 0;
     int i;
     char a[20];
     int t;
     heap *h = heap_nova(n);
-    for(i=0;i<n;i++) {
+    for (i = 0; i < n; i++) {
         strcpy(a, lista_elemento(acoes, i));
-        if(a == NULL) return 0;
+        if (a == NULL) return 0;
         heap_insere(h, a, atoi(lista_elemento(tempos, i)));
     }
     for (i = 0; i < n; i++) {
-        printf("%d: %s\n", i+1, h->elementos[i]->valor);
+        printf("%d: %s\n", i + 1, h->elementos[i]->valor);
     }
     return 1;
 }
 
-/* Aqui começa o código de teste. Não modificar! */ 
+/* Aqui começa o código de teste. Não modificar! */
 
-int main()
-{
-	/* teste problema 2.1 */
-	{
-		grafo *g=NULL;
-		lista *l=NULL;
-		int i;
+int main() {
+    /* teste problema 2.1 */
+    {
+        grafo *g = NULL;
+        lista *l = NULL;
+        int i;
 
-		printf("* Problema 2.1 *\n");
+        printf("* Problema 2.1 *\n");
 
-		g = grafo_novo(8, DIGRAFO);
+        g = grafo_novo(8, DIGRAFO);
 
-		grafo_adiciona(g, 1, 3);
-		grafo_adiciona(g, 2, 3);
-		grafo_adiciona(g, 2, 4);
-		grafo_adiciona(g, 2, 5);
-		grafo_adiciona(g, 3, 2);
-		grafo_adiciona(g, 3, 4);
-		grafo_adiciona(g, 4, 6);
-		grafo_adiciona(g, 4, 7);
-		grafo_adiciona(g, 5, 4);
-		grafo_adiciona(g, 5, 6);
-		grafo_adiciona(g, 5, 7);
-		grafo_adiciona(g, 6, 7);
+        grafo_adiciona(g, 1, 3);
+        grafo_adiciona(g, 2, 3);
+        grafo_adiciona(g, 2, 4);
+        grafo_adiciona(g, 2, 5);
+        grafo_adiciona(g, 3, 2);
+        grafo_adiciona(g, 3, 4);
+        grafo_adiciona(g, 4, 6);
+        grafo_adiciona(g, 4, 7);
+        grafo_adiciona(g, 5, 4);
+        grafo_adiciona(g, 5, 6);
+        grafo_adiciona(g, 5, 7);
+        grafo_adiciona(g, 6, 7);
 
-		l = descobre_caminho (g, 1, 7, 5);
+        l = descobre_caminho(g, 1, 7, 5);
 
-		printf("Descobre caminho - Inicio 1, Fim 7, Passando por 5\nCaminho: ");
-		for(i=0; i<lista_tamanho(l); i++)
-			printf("%s", lista_elemento(l, i));
-		printf("\n\n");
+        printf("Descobre caminho - Inicio 1, Fim 7, Passando por 5\nCaminho: ");
+        for (i = 0; i < lista_tamanho(l); i++)
+            printf("%s", lista_elemento(l, i));
+        printf("\n\n");
 
-		grafo_apaga(g);
-		lista_apaga(l);
-	}
+        grafo_apaga(g);
+        lista_apaga(l);
+    }
 
-	/* teste problema 2.2 */
-	{
-		FILE *f;
-		char acao[21], tempo[5];
-		lista *acoes, *tempos;
-		
-		printf("* Problema 2.2 *\n");
+    /* teste problema 2.2 */
+    {
+        FILE *f;
+        char acao[21], tempo[5];
+        lista *acoes, *tempos;
 
-		f = fopen("acoes.txt", "r");
-		acoes = lista_nova();
-		tempos = lista_nova();
+        printf("* Problema 2.2 *\n");
 
-		while(fscanf(f, "%s %s", acao, tempo) == 2)
-		{
-			lista_insere(acoes, acao, -1);
-			lista_insere(tempos, tempo, -1);
-		}
+        f = fopen("acoes.txt", "r");
+        acoes = lista_nova();
+        tempos = lista_nova();
 
-		if(simula_acontecimentos(acoes, tempos, 4) == 0)
-    		printf("Erro ao executar a funcao\n");
+        while (fscanf(f, "%s %s", acao, tempo) == 2) {
+            lista_insere(acoes, acao, -1);
+            lista_insere(tempos, tempo, -1);
+        }
 
-    	lista_apaga(acoes);
-    	lista_apaga(tempos);
-    	fclose(f);
-	}
+        if (simula_acontecimentos(acoes, tempos, 4) == 0)
+            printf("Erro ao executar a funcao\n");
 
-	return 0;
+        lista_apaga(acoes);
+        lista_apaga(tempos);
+        fclose(f);
+    }
+
+    return 0;
 }
